4df87b062d4ffda936cff675a2078868e6b12798
bael172 <bael171104@gmail.com>
2024-02-15 15:48:04 +0300

    2 stage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react'
-import {PropTypes} from "prop-types"
+import PropTypes from "prop-types"
+import './App.css'
 
 class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      txt:"trener"
+    }
+  }
+  update(){
+    this.setState({txt:event.target.value})
+  }
   render()
 {
   //return React.createElement('h1',null,'Hello nephews') React.createElement('b',null,'Bold')
-  let person = this.props.txt
+  const knopka = document.querySelector('button1')
   return (
-    <div>
-      <h1>Hello {person} and {this.props.cat}</h1>
+    <article>
+      <div>
+      <input type="text" onChange={this.update.bind(this)}/>
+      <h2>{this.state.txt}</h2>
+
     </div>
+      <div>
+        <button onclick="alert('Терпение')">Click me</button>
+        <script>
+            const my FirstEvent = document.querySelector('button')
+            myFirstEvent.onclick = function() {
+              alert("Да будет так!")
+            }
+            myFirstEvent.addEventListener('click', function(){
+              alert('Дорогу осилит идущий')
+            })
+        </script>
+      </div>
+    </article>
+    
   )
 }}
+/*
 App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
 }
 App.defaultProps = {
   txt: "this is the default txt"
 }
+*/
+
+      /*
+      document.getElementById('button1')
+      knopka.addEventListener('click', () => <noscript> You clicked </noscript>)
+      */
 
 //const App = () =><h1>Hello nephews!</h1>
 
